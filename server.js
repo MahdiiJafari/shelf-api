6229500f1ecd99d3852b1dc4bbf14236996f4a7d
mahdi <mahdi.jafarii.804@gmail.com>
2026-01-07 12:41:25 +0330

    implemented basic structure of server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./.enc" });
+const app = require("./src/app");
+
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose.connect(DB, {}).then(() => console.log("DB connection successful!"));
+
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const bookRouter = require("./routes/bookRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
