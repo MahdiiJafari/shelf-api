0023338be69936245a27cc1c9917a47fd9169edc
mahdi <mahdi.jafarii.804@gmail.com>
2026-01-07 16:56:01 +0330

    implemented book controller

---
+++ src/controllers/bookController.js
@@ -0,0 +1,67 @@
+const Book = require("../models/bookModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllBooks = catchAsync(async (req, res, next) => {
+  const books = await Book.find();
+
+  res.status(200).json({
+    status: "success",
+    results: books.length,
+    data: { books },
+  });
+});
+
+exports.getBook = catchAsync(async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+
+  if (!book) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No book found with that ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { book },
+  });
+});
+
+exports.updateBook = catchAsync(async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+
+  if (!book) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No book found with that ID",
+    });
+  }
+
+  const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: { book: updatedBook },
+  });
+});
+
+exports.deleteBook = catchAsync(async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+
+  if (!book) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No book found with that ID",
+    });
+  }
+
+  await Book.findByIdAndDelete(req.params.id);
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
