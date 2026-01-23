0382558e24064afa528ece41ff577fe634c2f335
mahdi <mahdi.jafarii.804@gmail.com>
2026-01-23 21:29:34 +0330

    implemented pagination

--- src/utils/apiFeatures.js
+++ src/utils/apiFeatures.js
@@ -5,6 +5,7 @@ class APIFeatures {
     this.query = query;
     this.queryString = queryString;
   }
+  
   filter() {
     const queryObj = { ...this.queryString };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
@@ -17,6 +18,7 @@ class APIFeatures {
 
     return this;
   }
+
   sort() {
     if (this.queryString.sort) {
       const sortBy = this.queryString.sort.split(',').join(' ');
@@ -26,15 +28,26 @@ class APIFeatures {
     }
     return this;
   }
+
   limitFields() {
     if (this.queryString.fields) {
       const fields = this.queryString.fields.split(',').join(' ');
       console.log(fields);
-      
+
       this.query = this.query.select(fields);
     } else {
       this.query = this.query.select('-__v');
     }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
+
     return this;
   }
 }
